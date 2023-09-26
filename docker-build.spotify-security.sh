2efc0e1c399f9ca8cbbd6454c90b4f0bef7d9440
bas <schipper.b@outlook.com>
2023-09-26 11:05:22 +0200

    tussentijdse commit, maven handmatig installeren in dockerfile + docker-build script

---
+++ docker-build.spotify-security.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+IMAGE_TAG="github.com/b-schipper/spotify-security"
+VERSION="${1-latest}"
+
+# script takes an optional tag argument, otherwise uses "latest"
+docker build \
+    -f spotify-security.Dockerfile \
+    -t "$IMAGE_TAG:$VERSION" .
+
+# workaround for skaffold; unused, as skaffold does not know about compute jobs
+if [ -n "$IMAGE" ]; then
+    docker tag "$IMAGE_TAG:$VERSION" "$IMAGE"
+fi
+
+echo "$IMAGE_TAG:$VERSION"
